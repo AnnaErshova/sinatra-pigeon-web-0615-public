a2e6435c20fe4a6f8e288ec21f4d37a11096be0b
AnnaErshova <anna.ershova@gmail.com>
2015-06-29 15:15:23 -0400

    all done

--- app/controllers/pigeons_controller.rb
+++ app/controllers/pigeons_controller.rb
@@ -1,2 +1,55 @@
 # create your PigeonsController here
 # it should inherit from ApplicationController
+
+class PigeonsController < ApplicationController
+
+  get '/pigeons' do
+    erb :"pigeons/index"
+  end
+  # renders the pigeon index template
+  # list all projects
+    #  pigeon = Pigeon.new
+    #pigeon.name = params[:name]
+    #pigeon.color = params[:name]
+    #pigeon.gender = params[:name]
+    #pigeon.lives = params[:name]
+    #pigeon.save
+
+  get '/pigeons/new' do # new comes before :id
+    erb :"pigeons/new"
+  end 
+    
+  get '/pigeons/:id' do |id|
+    @pigeon = Pigeon.find(id)
+    erb :"pigeons/show"
+  end
+  # renders the show page for the pigeons's list of attributes
+  # shows a particular project
+
+  # renders the new pigeon form
+  # expected "" to include "</form>"
+
+  get '/pigeons/:id/edit' do |id|
+    @pigeon = Pigeon.find(id)
+    erb :"pigeons/edit"
+  end
+  # renders the edit page for the pigeon
+
+  post '/pigeons' do
+    Pigeon.create(params)
+    redirect '/pigeons'
+  end
+
+  #
+  delete '/pigeons/:id' do |id|
+    Pigeon.destroy(id)
+    redirect '/pigeons'
+  end
+
+  patch '/pigeons/:id' do |id|
+    Pigeon.update(id, {name: params[:name], color: params[:color], gender: params[:gender], lives: params[:lives]})
+    redirect "/pigeons/#{id}"
+  end
+
+end
+
