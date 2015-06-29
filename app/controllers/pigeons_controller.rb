c6d6f0c895dd3a5eda9aeb0251cb93bebb3854b7
AnnaErshova <anna.ershova@gmail.com>
2015-06-29 15:27:51 -0400

    improves code

--- app/controllers/pigeons_controller.rb
+++ app/controllers/pigeons_controller.rb
@@ -1,53 +1,51 @@
-# create your PigeonsController here
 # it should inherit from ApplicationController
-
 class PigeonsController < ApplicationController
 
+  # renders the pigeon index template / list all project
   get '/pigeons' do
     erb :"pigeons/index"
   end
-  # renders the pigeon index template
-  # list all projects
-    #  pigeon = Pigeon.new
+
+    #pigeon = Pigeon.new
     #pigeon.name = params[:name]
-    #pigeon.color = params[:name]
+    #pigeon.color = params[:color]
     #pigeon.gender = params[:name]
     #pigeon.lives = params[:name]
     #pigeon.save
 
-  get '/pigeons/new' do # new comes before :id
+  # /new comes before /:id; gets messed up otherwise
+  get '/pigeons/new' do 
     erb :"pigeons/new"
   end 
-    
+  
+  # shows attributes of one pigeon
   get '/pigeons/:id' do |id|
     @pigeon = Pigeon.find(id)
     erb :"pigeons/show"
   end
-  # renders the show page for the pigeons's list of attributes
-  # shows a particular project
 
-  # renders the new pigeon form
-  # expected "" to include "</form>"
-
+  # renders edit page for one pigeon
   get '/pigeons/:id/edit' do |id|
     @pigeon = Pigeon.find(id)
     erb :"pigeons/edit"
   end
-  # renders the edit page for the pigeon
 
   post '/pigeons' do
     Pigeon.create(params)
     redirect '/pigeons'
   end
 
-  #
   delete '/pigeons/:id' do |id|
     Pigeon.destroy(id)
     redirect '/pigeons'
   end
 
   patch '/pigeons/:id' do |id|
-    Pigeon.update(id, {name: params[:name], color: params[:color], gender: params[:gender], lives: params[:lives]})
+    Pigeon.update(id, {
+      name: params[:name], 
+      color: params[:color], 
+      gender: params[:gender], 
+      lives: params[:lives]})
     redirect "/pigeons/#{id}"
   end
 
